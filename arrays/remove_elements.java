8ea25a89ab4417eb8a214eeb34055029cc82d790
Tejaswini1734 <tejaswini.v317@gmail.com>
2025-04-01 10:05:56 +0530

    added remove elements code

---
+++ arrays/remove_elements.java
@@ -0,0 +1,44 @@
+
+import java.util.Scanner;
+
+public class remove_Element {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements: ");
+        int n = scanner.nextInt();
+        int[] nums = new int[n];
+
+        System.out.println("Enter the elements:");
+        for (int i = 0; i < n; i++) {
+            nums[i] = scanner.nextInt();
+        }
+
+        System.out.print("Enter the value to remove: ");
+        int val = scanner.nextInt();
+
+        Solution solution = new Solution();
+        int k = solution.removeElement(nums, val);
+
+        System.out.println("Updated array length: " + k);
+        System.out.print("Updated array: ");
+        for (int i = 0; i < k; i++) {
+            System.out.print(nums[i] + " ");
+        }
+
+        scanner.close();
+    }
+}
+
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int k = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+}
